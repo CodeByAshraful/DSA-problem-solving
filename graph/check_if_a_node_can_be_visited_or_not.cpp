ff9d011618adeb731a5ef484a382de25c2f0a683
Ashraful Khan <aponkhanbd884@gmail.com>
2025-12-19 15:35:52 +0600

    Add files via upload
    This file demonstrates a basic graph traversal concept.
    It checks whether a given node in a graph can be visited using standard
    graph traversal techniques such as BFS.

---
+++ graph/check_if_a_node_can_be_visited_or_not.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>adj_list[1005];
+bool vis[1005];
+
+void check_vis_node(int src)
+ {
+    queue<int>q;
+    q.push(src);
+    vis[src]=true;
+    while(!q.empty())
+    {
+        int par = q.front();
+        q.pop();
+        for(int child : adj_list[par])
+        {
+           if(vis[child]==false)
+           {
+              q.push(child);
+              vis[child]=true;
+           }
+            
+        }
+    }
+ }
+int main()
+{
+    int n,e;
+    cin>>n>>e;
+    while(e--)
+    {
+        int a,b;
+        cin>>a>>b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);   
+    }
+    memset(vis,false,sizeof(vis));
+    int src,des; // src->source node; des->destination;
+    cin>>src>>des;
+
+    check_vis_node(src);
+    //checking visted or not
+    if(vis[des])
+    {
+        cout<<"Yes";
+    }
+    else
+    cout<<"No"; 
+
+   return 0;
+}
