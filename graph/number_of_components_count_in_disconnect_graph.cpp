285fe50eb3268c43638ea6ae893f6b4e5dd8c4b4
Ashraful Khan <aponkhanbd884@gmail.com>
2025-12-30 00:30:07 +0600

    Add files via upload
    added count of graph components code.

---
+++ graph/number_of_components_count_in_disconnect_graph.cpp
@@ -0,0 +1,58 @@
+/*
+A graph component is a group of nodes where every node 
+is connected (directly or indirectly) to each other,
+ and no connection exists with nodes outside the group.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>adj_list[1005];
+bool vis[105];
+void dfs(int src)
+{
+    vis[src]=true;
+    for(int child : adj_list[src])
+    {
+        if(!vis[child])
+        {
+           dfs(child);
+        }
+    }
+      
+}
+int main()
+{
+    int n,e;
+    cin>>n>>e;
+    while(e--)
+    {
+       int a,b;
+       cin>>a>>b;
+       adj_list[a].push_back(b);
+       adj_list[b].push_back(a);
+    }
+    memset(vis,false,sizeof(vis));
+    int count = 0;
+    for(int i=0; i<n; i++)
+    {
+        if(vis[i]==false)
+        {
+            dfs(i);
+            count++;
+        }
+    }
+    cout<<count<<endl;
+
+    return 0;
+}
+/*
+sample input : 
+8 6
+1 2
+0 5
+2 3
+6 7
+4 5
+1 3
+sample output : 
+3
+*/
